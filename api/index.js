e36570a6652aa9bbc999a1c1bbb8317115cbd661
EdgarVelasco1 <edgar.velasco@epn.edu.ec>
2024-07-22 21:24:28 -0500

    configuraciones previas aun no probadas

--- api/index.js
+++ api/index.js
@@ -1,9 +1,27 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGODB_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+
+//creamos un nuevo modelo
+
+const Users = mongoose.model('User', new mongoose.Schema({ nombre: String}))
+
+Users.create({nombre: 'Chanchito Feliz'})
 
 app.get(('*'),(request, response) => {
-    response.send({message : 'Hola Mundo'})
-    console.log('mi primera aplicacion de Almuerzi')
+
+    Users.find()
+        .then(x=> response.send(x))
+    // response.send({message : 'Hola Mundo'})
+    // console.log('mi primera aplicacion de Almuerzi')
 })
 
 module.exports = app

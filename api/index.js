4f1d23e1c96853f226de4ddabba70e342907f052
EdgarVelasco1 <edgar.velasco@epn.edu.ec>
2024-07-25 12:55:27 -0500

    corrección  query

--- api/index.js
+++ api/index.js
@@ -11,14 +11,18 @@ mongoose.connect(process.env.MONGODB_URI)
 
 const Users = mongoose.model('User', new mongoose.Schema({ nombre: String}))
 
-Users.create({nombre: 'Chanchito Triste'})
-
-app.get(('*'),(request, response) => {
-
-    Users.find()
-        .then(x=> response.send(x))
-    // response.send({message : 'Hola Mundo'})
-    // console.log('mi primera aplicacion de Almuerzi')
-})
+// Ruta para crear un nuevo usuario
+app.get('/api', async (req, res) => {
+    try {
+      const user = await Users.create({ nombre: 'Chanchito Final' });
+      console.log('User created:', user);
+      const users = await Users.find();
+      res.send(users);
+    //   res.send({ message: 'User created successfully' });
+    } catch (error) {
+      console.error('Error creating user:', error);
+      res.status(500).send({ error: 'Internal Server Error' });
+    }
+  });
 
 module.exports = app
